1efd5fc7c643d4de99262af797db273642e41e0e
Devair Luiz Moitim <dmoitim@gmail.com>
2021-01-11 17:18:23 -0300

    Inicio da implementacao da lista e qtd de EPs

--- controle-series/resources/views/temporadas/index.blade.php
+++ controle-series/resources/views/temporadas/index.blade.php
@@ -13,8 +13,13 @@ Temporadas da série {{ $serie->nome }}
 
 <ul class="list-group">
     @foreach ($temporadas as $temporada)
-    <li class="list-group-item">
-        Temporada {{ $temporada->numero }}
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <a href="#">
+            Temporada {{ $temporada->numero }}
+        </a>
+        <span class="badge badge-secondary">
+            0 / {{ $temporada->episodios->count() }}
+        </span>
     </li>
     @endforeach
 </ul>
